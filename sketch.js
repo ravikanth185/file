a026447244895a2fe23cff4af5401e7364a3e0a7
ravikanth185 <73772728+ravikanth185@users.noreply.github.com>
2021-02-07 17:27:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var hypnoticBall, database;
+var gamestate,playercount;
+var form,game,player;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+}
+function draw(){
+  background("white");
+  
+  
+  
+}
+
